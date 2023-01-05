939190a1db4d865220d98c59a271ed8d9db471dc
Deepak <dk950046@gmail.com>
2023-01-05 16:16:57 +0530

    initial commit

--- custom_theme/hooks.py
+++ custom_theme/hooks.py
@@ -11,12 +11,12 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/custom_theme/css/custom_theme.css"
-# app_include_js = "/assets/custom_theme/js/custom_theme.js"
+app_include_css = "/assets/custom_theme/css/custom_theme.css"
+app_include_js = "/assets/custom_theme/js/desk_script.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/custom_theme/css/custom_theme.css"
-# web_include_js = "/assets/custom_theme/js/custom_theme.js"
+web_include_css = "/assets/custom_theme/css/website_theme.css"
+web_include_js = "/assets/custom_theme/js/website_script.js"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "custom_theme/public/scss/website"
